3f7364cb36818174597fa14919c0cd6166f2149e
riy <riley@ahbert.com>
2026-02-01 13:38:28 +0000

    revamp

---
+++ landing/landing1.js
@@ -0,0 +1,22 @@
+//timer
+const startTime = new Date("2026-08-21T12:00:00Z");
+const endTime = new Date("2026-08-24T12:00:00Z");
+
+function updateCountdown() {
+    const now = new Date();
+    if (now <= endTime) {
+        const diff = startTime - now;
+            
+        const days = Math.floor(diff / (1000 * 60 * 60 * 24));
+        const hours = Math.floor((diff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        const minutes = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60));
+        const seconds = Math.floor((diff % (1000 * 60)) / 1000);
+        document.getElementById("days").textContent = String(days).padStart(2, "0");
+        document.getElementById("hours").textContent = String(hours).padStart(2, "0");
+        document.getElementById("minutes").textContent = String(minutes).padStart(2, "0");
+        document.getElementById("seconds").textContent = String(seconds).padStart(2, "0");    
+    }
+}
+
+updateCountdown();
+setInterval(updateCountdown, 1000);
